e8fcf45ddc1d6c91fabbdf42f7bf0923d3d0aeec
Ywwwxxx <yusufturandemir@hotmail.com>
2025-12-30 19:52:07 +0300

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -1,12 +1,25 @@
-const tf = require('@tensorflow/tfjs-node');
+import * as tf from '@tensorflow/tfjs';
 
-// model klasörünü kök dizinde tutuyorsun
-let modelPromise = tf.loadLayersModel('file://./model/model.json');
+// Modeli model klasöründen yükle
+const modelPromise = tf.loadLayersModel('file://./model/model.json');
 
 export default async function handler(req, res) {
-  const { input } = req.query;
-  const model = await modelPromise;
-  const prediction = model.predict(tf.tensor([JSON.parse(input)])).dataSync();
-  res.status(200).json({ result: prediction });
+  try {
+    const inputRaw = req.query.input;
+    if (!inputRaw) {
+      return res.status(400).json({ error: 'input parametresi gerekli' });
+    }
 
+    const input = JSON.parse(inputRaw); // örn: [1,0,0,1]
+    const model = await modelPromise;
+
+    const tensor = tf.tensor([input]); // [batch, features]
+    const pred = model.predict(tensor);
+
+    const result = Array.from(await pred.data());
+    res.status(200).json({ result });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
 }
+
