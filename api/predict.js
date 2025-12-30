f901bb24b24c393ffcfcc7414f38ead16c711849
Ywwwxxx <yusufturandemir@hotmail.com>
2025-12-30 19:46:56 +0300

    Update predict.js

--- api/predict.js
+++ api/predict.js
@@ -1,11 +1,12 @@
 const tf = require('@tensorflow/tfjs-node');
 
 // model klasörünü kök dizinde tutuyorsun
-let modelPromise = tf.loadLayersModel('file://model/model.json');
+let modelPromise = tf.loadLayersModel('file://./model/model.json');
 
 export default async function handler(req, res) {
   const { input } = req.query;
   const model = await modelPromise;
   const prediction = model.predict(tf.tensor([JSON.parse(input)])).dataSync();
   res.status(200).json({ result: prediction });
+
 }
